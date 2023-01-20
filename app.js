3d373c7e39f003230e7bb0803a736f7d1f9f7905
Marko1310 <marko.cabo13@gmail.com>
2023-01-20 09:07:07 +0100

    Implementing third party middleware

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+const { log } = require("console");
 const express = require("express");
 const fs = require("fs");
+const morgan = require("morgan");
 
 const app = express();
 
+// 1) MIDDLEWARES
+app.use(morgan("dev"));
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("Hello from the middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -11,8 +26,10 @@ const tours = JSON.parse(
 
 ////// FUNCTIONS //////
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: "success",
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours: tours,
@@ -88,13 +105,13 @@ const deleteTour = (req, res) => {
   });
 };
 
-///////// ROUTING ///////////
-
 // app.get("/api/v1/tours", getAllTours);
 // app.post("/api/v1/tours", createTour);
 // app.get("/api/v1/tours/:id", getTour);
 // app.patch("/api/v1/tours/:id", changeTour);
 // app.delete("/api/v1/tours/:id", deleteTour);
+
+// 3) ROUTES
 
 app.route("/api/v1/tours").get(getAllTours).post(createTour);
 
