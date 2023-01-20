32d1e5e7a0b1f431d1ef48fb5b420b6e3cb41ca0
Marko1310 <marko.cabo13@gmail.com>
2023-01-20 14:14:41 +0100

    Chaining multiple middleware functions

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const tourController = require("./../controllers/tourController");
-
 const router = express.Router();
 
 router.param("id", tourController.checkID);
@@ -8,7 +7,7 @@ router.param("id", tourController.checkID);
 router
   .route("/")
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 router
   .route("/:id")
   .get(tourController.getTour)
